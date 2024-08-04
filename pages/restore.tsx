59a9de0fe927bbe1d1ffc6d194408906f4b1a19d
dailituweb <135852430+dailituweb@users.noreply.github.com>
2024-08-04 12:05:25 +0800

    Update restore.tsx

--- pages/restore.tsx
+++ pages/restore.tsx
@@ -111,7 +111,7 @@ const Home: NextPage = () => {
   return (
     <div className='flex max-w-6xl mx-auto flex-col items-center justify-center py-2 min-h-screen'>
       <Head>
-        <title>Restore Photos</title>
+        <title>Photo Enhancer - Aiphotoen</title>
         <link rel='icon' href='/favicon.ico' />
       </Head>
 
